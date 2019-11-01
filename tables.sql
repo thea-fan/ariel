7377ce9e47bf2b82f381f10d47055ceae289b2a0
thea-fan <julythefourteen@gmail.com>
2019-11-01 10:15:55 +0800

    added answer_id column to questions table

--- tables.sql
+++ tables.sql
@@ -1,46 +1,22 @@
 
 
-CREATE TABLE IF NOT EXISTS activity (
-    id SERIAL PRIMARY KEY,
-    host_id INTEGER,
-    host_postal INTEGER,
-    type TEXT,
-    name TEXT,
-    max_pax INTEGER,
-    event_address TEXT,
-    event_postal INTEGER,
-    event_description TEXT,
-    event_photo TEXT,
-    event_date DATE,
-    start_time TEXT,
-    end_time TEXT,
-    created_at DATE DEFAULT now(),
-    active BOOLEAN DEFAULT true
-);
-
-CREATE TABLE IF NOT EXISTS respondent (
-	id SERIAL PRIMARY KEY,
-  activity_id INTEGER,
-  respondent_id INTEGER,
-  respondent_name TEXT
-);
-
 CREATE TABLE IF NOT EXISTS questions (
 	qn_id SERIAL PRIMARY KEY,
 	question_title TEXT,
 	equipment TEXT,
 	question_text TEXT,
 	question_photo  TEXT,
-	user_id INT,
+	user_id INT REFERENCES users(id),
 	question_status TEXT,
+	answer_id INT REFERENCES replies(id) DEFAULT null,
 	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS replies (
 	id SERIAL PRIMARY KEY,
-	replied_user_id INT,
-	question_id INT,
+	replied_user_id REFERENCES users(id),
+	question_id REFERENCES questions(qn_id),
 	reply_text TEXT,
 	reply_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
